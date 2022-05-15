cbbc9485cac58db831ae68f5558a382169537112
Austin6868 <60564361+Austin6868@users.noreply.github.com>
2022-05-15 16:54:57 -0500

    change directory

--- Logi_App/views.py
+++ Logi_App/views.py
@@ -8,28 +8,28 @@ from django.views.generic import ListView, DetailView, UpdateView, CreateView, D
 # Create your views here.
 class IndexView(ListView):
     model = Inv_Item
-    template_name = 'Logi_App/index.html'
+    template_name = 'Logi_App/../templates/index.html'
     context_object_name = 'post_list'
 
 class SingleView(DetailView):
     model = Inv_Item
-    template_name = 'Logi_App/single.html'
+    template_name = 'Logi_App/../templates/single.html'
     context_object_name = 'post'
 
 class PostView(ListView):
     model = Inv_Item
-    template_name = "Logi_App/posts.html"
+    template_name = "Logi_App/../templates/posts.html"
     context_object_name = 'post_list'
 
 class AddView(CreateView):
     model = Inv_Item
-    template_name = 'Logi_App/add.html'
+    template_name = 'Logi_App/../templates/add.html'
     fields = '__all__'
     success_url = reverse_lazy('Logi_App:posts')
 
 class EditView(UpdateView):
     model = Inv_Item
-    template_name = 'Logi_App/edit.html'
+    template_name = 'Logi_App/../templates/edit.html'
     fields = '__all__'
     pk_url_kwarg = 'pk'
     success_url = reverse_lazy('Logi_App:posts')
@@ -38,11 +38,11 @@ class Delete(DeleteView):
     model = Inv_Item
     pk_url_kwarg = 'pk'
     success_url = reverse_lazy('Logi_App:posts')
-    template_name = 'Logi_App/confirm-delete.html'
+    template_name = 'Logi_App/../templates/confirm-delete.html'
 
 class AddWarehouse(CreateView):
     model = Warehouse
-    template_name = 'Logi_App/add_warehouse.html'
+    template_name = 'Logi_App/../templates/add_warehouse.html'
     fields = '__all__'
     success_url = reverse_lazy('Logi_App:posts_warehouse')
 
@@ -50,10 +50,10 @@ class DeleteWarehouse(DeleteView):
     model = Warehouse
     pk_url_kwarg = 'pk'
     success_url = reverse_lazy('Logi_App:posts_warehouse')
-    template_name = 'Logi_App/confirm-delete.html'
+    template_name = 'Logi_App/../templates/confirm-delete.html'
 
 class PostWarehouse(ListView):
     model = Warehouse
-    template_name = "Logi_App/posts_warehouse.html"
+    template_name = "Logi_App/../templates/posts_warehouse.html"
     context_object_name = 'post_list'
 
